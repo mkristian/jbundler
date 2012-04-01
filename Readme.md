07859f95bb941928b1d423091f34d7ee9cfdd1f6
Kristian Meier <m.kristian@web.de>
2012-04-02 01:54:17 +0530

    proof of concept

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# jbundler #
+
+just add it as **first** entry in your *Gemfile* (pending since no gem is released)
+
+```gem 'jbundler'```
+
+when having this lib directory on the loadpath just
+
+```require 'jbundler'```
+
+or run it directly
+
+```jruby -Ilib lib/jbundler.rb```
+
+to get the classpath setup in *.jbundler/classpath*
---
+++ lib/jbundler.rb
@@ -0,0 +1,14 @@
+require 'jbundler/maven'
+
+mavenfile = 'Mvnfile'
+classpathfile = '.jbundler/classpath.rb'
+if !File.exists?(classpathfile) || (File.mtime(mavenfile) > File.mtime(classpathfile))
+  JBundler::Maven.new.generate_classpath(mavenfile,classpathfile)
+end
+if File.exists?(classpathfile)
+  require 'java'
+  require classpathfile 
+  true
+else
+  false
+end
