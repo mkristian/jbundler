a01cf8efcdd82c8e14d470d22753476437c4c0d5
Christian Meier <m.kristian@web.de>
2014-07-08 17:29:21 +0100

    reuse vendored thor from bundler - fixes #41

--- lib/jbundler/cli.rb
+++ lib/jbundler/cli.rb
@@ -18,7 +18,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-require 'thor'
+require 'bundler/vendor/thor'
 require 'jbundler/config'
 require 'jbundler/executable'
 require 'jbundler/tree'
