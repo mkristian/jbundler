0b7540df164304b3f4e5b7df831c7f94eccecb07
mluu <mluu@lancope.com>
2014-08-07 15:03:02 -0700

    pass local repository configuration to ruby-maven

--- lib/jbundler/configurator.rb
+++ lib/jbundler/configurator.rb
@@ -7,7 +7,7 @@ module JBundler
     def initialize( config )
       @config = config
     end
-    
+
     def configure( maven )
       maven.property( 'jbundler.basedir', @config.basedir )
       maven.property( 'jbundler.jarfile', @config.jarfile )
@@ -15,6 +15,7 @@ module JBundler
       maven.property( 'jbundler.workdir', work_dir )
       maven.property( 'jbundler.groups', @groups )
       maven.property( 'jbundler.bootstrap', @bootstrap )
+      maven.property( 'maven.repo.local', @config.local_repository )
     end
 
     def work_dir
