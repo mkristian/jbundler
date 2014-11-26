6a40f35751fd67971986b120a510b221562e31ba
Christian Meier <m.kristian@web.de>
2014-11-26 20:40:48 +0000

    adde some comment

--- integration/gemspec_with_system_jar/gem/lib/first_jars.rb
+++ integration/gemspec_with_system_jar/gem/lib/first_jars.rb
@@ -1,5 +1,6 @@
 # this is a generated file, to avoid over-writing it just delete this comment
 require 'jar_dependencies'
 
+# something which will be overwritten anyways
 require_jar( 'org.bouncycastle', 'bcpkix-jdk15on', '1.49' )
 require_jar( 'org.bouncycastle', 'bcprov-jdk15on', '1.49' )
