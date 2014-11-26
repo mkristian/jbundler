90e74d3aeeef74f9d7f5960a2e4e2b8d10213b83
Christian Meier <m.kristian@web.de>
2014-11-26 20:41:05 +0000

    added test for https://github.com/jruby/jruby/issues/2079
    this finally fixes #52
    
    Sponsored by Lookout Inc.

---
+++ integration/running_rspec_via_rake/spec/routing/users_routing_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+describe UsersController do
+  describe "routing" do
+
+    it "routes to #index" do
+      get("/users").should route_to("users#index")
+    end
+
+    it "routes to #new" do
+      get("/users/new").should route_to("users#new")
+    end
+
+    it "routes to #show" do
+      get("/users/1").should route_to("users#show", :id => "1")
+    end
+
+    it "routes to #edit" do
+      get("/users/1/edit").should route_to("users#edit", :id => "1")
+    end
+
+    it "routes to #create" do
+      post("/users").should route_to("users#create")
+    end
+
+    it "routes to #update" do
+      put("/users/1").should route_to("users#update", :id => "1")
+    end
+
+    it "routes to #destroy" do
+      delete("/users/1").should route_to("users#destroy", :id => "1")
+    end
+
+  end
+end
---
+++ integration/running_rspec_via_rake/spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "users/new" do
+  before(:each) do
+    assign(:user, stub_model(User,
+      :name => "MyString"
+    ).as_new_record)
+  end
+
+  it "renders new user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", users_path, "post" do
+      assert_select "input#user_name[name=?]", "user[name]"
+    end
+  end
+end
