2396bc2873acae78a79e4d50639daf346d9f7418
Christian Meier <m.kristian@web.de>
2014-12-04 13:02:58 +0000

    do not inherit BUNDLER_GEMFILE from outer travis run
    travis sets BUNDLER_GEMFILE environment variable which
    gets picked up by some integration tests running ```jbundle install```
    instead of the supplied Gemfile of the test.
    
    Sponsored by Lookout Inc.

--- integration/give_priority_to_jars_from_jarfile/test.rb
+++ integration/give_priority_to_jars_from_jarfile/test.rb
@@ -5,6 +5,6 @@ require 'jbundler'
 JBundler.install
 JBundler.setup
 
-raise "missing kafka_2.10-0.8.1.1.jar" unless $CLASSPATH.detect { |c| c =~ %r(/org/apache/kafka/kafka_2.10/0.8.1.1/kafka_2.10-0.8.1.1.jar) }
+raise "missing kafka_2.10-0.8.2-beta.jar" unless $CLASSPATH.detect { |c| c =~ %r(/org/apache/kafka/kafka_2.10/0.8.2-beta/kafka_2.10-0.8.2-beta.jar) }
 
 # vim: syntax=Ruby
